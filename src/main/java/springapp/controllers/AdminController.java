b6070325035549f834d330eecbc557eaa1bfd0ac
Kamal <kamal12325@mail.ru>
2024-08-27 17:48:53 +0300

    all_sql_version_0.1.3(some new fiches)

--- src/main/java/springapp/controllers/AdminController.java
+++ src/main/java/springapp/controllers/AdminController.java
@@ -1,5 +1,35 @@
 package springapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import springapp.dao.PersonDAO;
+import springapp.models.Person;
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public AdminController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String getAdminPage(Model model, @ModelAttribute Person person) {
+        model.addAttribute("people", personDAO.index());
+
+        return "adminPage";
+    }
+
+    @PatchMapping("/add")
+    public String makeAdmin(@ModelAttribute("person") Person person){
+        System.out.println(person.getId());
+
+        return "redirect:/people";
+    }
 
 }
